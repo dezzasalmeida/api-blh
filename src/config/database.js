2e919cc5bcaa3c81bfd6a251741c40e507d947ee
Andrezza <dezzasalmeida@gmail.com>
2024-08-12 10:28:44 -0300

    database config

--- src/config/database.js
+++ src/config/database.js
@@ -8,6 +8,14 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT || 5432,
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
   }
 );
 
